6c57de011ffe0d8cb91c8080f6f3722c5a359524
dsig0 <75738471+dsig0@users.noreply.github.com>
2025-11-07 18:31:26 +0530

    fix Default network error

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   # Install necessary packages
   environment.systemPackages = with pkgs; [
     virt-manager
@@ -12,13 +11,25 @@
     adwaita-icon-theme
   ];
 
+  networking.firewall.trustedInterfaces = [ "virbr0" ];
+  systemd.services.libvirt-default-network = {
+    description = "Start libvirt default network";
+    after = [ "libvirtd.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.libvirt}/bin/virsh net-start default";
+      ExecStop = "${pkgs.libvirt}/bin/virsh net-destroy default";
+      User = "root";
+    };
+  };
+
   # Manage the virtualisation services
   virtualisation = {
     libvirtd = {
       enable = true;
-      qemu = {
-        swtpm.enable = true;
-      };
+      qemu = { swtpm.enable = true; };
     };
     spiceUSBRedirection.enable = true;
   };
